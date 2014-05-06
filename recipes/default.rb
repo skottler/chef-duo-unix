8b20111057646ac0dc6a8c58cc41eed5b46776f4
Sam Kottler <shk@linux.com>
2014-05-06 10:11:13 +0600

    Disable PermitTunnel and AllowTcpForwarding in sshd_config
    The problem is that you can bypass two-factor authentication on certain operations when using ForceCommand to prompt the user to enter their OTP after successful initial auth via password/key/GSSAPI. ForceCommand doesn't prevent port forwarding from taking place after initial authentication, which means a user who has gotten access to a machine over brute-force or another unauthorized method can get access to ports which are firewalled or listening only on a loopback interface.
    
    Here's how to reproduce the issue:
    
    1. Have a machine with two-factor enabled using ForceCommand to prompt the user for their second form of auth.
    2. Have a service listening on a TCP socket which is not available to the 'outside world'
    3. ssh -L 8080:localhost:8080 me@mymachine.com (assuming the service is listening on 127.0.0.1:8080)
    4. Log into the machine via SSH key or password, but do not enter your OTP
    5. Notice that you can access the service on your local machine on port 8080 without completing two-factor authentication
    
    Disabling these two options eliminates the possibility of bypassing the second form of auth.

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,12 +19,12 @@
 
 case node[:platform]
 when "debian", "ubuntu"
-	package "libssl-dev" do
-		action :upgrade
-	end
-	package "make" do
-		action :upgrade
-	end
+  package "libssl-dev" do
+    action :upgrade
+  end
+  package "make" do
+    action :upgrade
+  end
 end
 
 #install duo_unix from source
@@ -55,50 +55,54 @@ end
 
 #enable login_duo for ssh
 if node['duo_unix']['conf']['login_duo_enabled'] 
-	bash "require login_duo for ssh" do
-		code <<-WTAF
-		grep -q 'ForceCommand /usr/sbin/login_duo' /etc/ssh/sshd_config || echo 'ForceCommand /usr/sbin/login_duo' >> /etc/ssh/sshd_config
-		WTAF
-	end
+  bash "require login_duo for ssh" do
+    code <<-WTAF
+    grep -q 'ForceCommand /usr/sbin/login_duo' /etc/ssh/sshd_config || echo 'ForceCommand /usr/sbin/login_duo' >> /etc/ssh/sshd_config
+    grep -q 'PermitTunnel no' /etc/ssh/sshd_config || echo 'PermitTunnel no' >> /etc/ssh/sshd_config
+    grep -q 'AllowTcpForwarding no' /etc/ssh/sshd_config || echo 'AllowTcpForwarding no' >> /etc/ssh/sshd_config
+    WTAF
+  end
 else
-	bash "disable login_duo for ssh" do
-		code <<-WTAF
-		sed -i '\#ForceCommand /usr/sbin/login_duo#d' /etc/ssh/sshd_config
-		WTAF
-	end
+  bash "disable login_duo for ssh" do
+    code <<-WTAF
+    sed -i '\#ForceCommand /usr/sbin/login_duo#d' /etc/ssh/sshd_config
+    sed -i '\#PermitTunnel no#d' /etc/ssh/sshd_config
+    sed -i '\#AllowTcpForwarding no#d' /etc/ssh/sshd_config
+    WTAF
+  end
 end
 
 #hacky way of editing the sshd_config file to add or remove lines
 if node['duo_unix']['conf']['PermitTunnel'] 
-	bash "remove permit tunnel no" do
-		code <<-WTAF
-		sed -i '\#PermitTunnel no#d' /etc/ssh/sshd_config
-		WTAF
-	end
+  bash "remove permit tunnel no" do
+    code <<-WTAF
+    sed -i '\#PermitTunnel no#d' /etc/ssh/sshd_config
+    WTAF
+  end
 else
-	bash "add permit tunnel no" do
-		code <<-WTAF
-		grep -q 'PermitTunnel no' /etc/ssh/sshd_config || echo 'PermitTunnel no' >> /etc/ssh/sshd_config
-		WTAF
-	end
+  bash "add permit tunnel no" do
+    code <<-WTAF
+    grep -q 'PermitTunnel no' /etc/ssh/sshd_config || echo 'PermitTunnel no' >> /etc/ssh/sshd_config
+    WTAF
+  end
 end
 
 if node['duo_unix']['conf']['AllowTcpForwarding'] 
-	bash "remove AllowTcpForwarding no" do
-		code <<-WTAF
-		sed -i '\#AllowTcpForwarding no#d' /etc/ssh/sshd_config
-		WTAF
-	end
+  bash "remove AllowTcpForwarding no" do
+    code <<-WTAF
+    sed -i '\#AllowTcpForwarding no#d' /etc/ssh/sshd_config
+    WTAF
+  end
 else
-	bash "add AllowTcpForwarding no" do
-		code <<-WTAF
-		grep -q 'AllowTcpForwarding no' /etc/ssh/sshd_config || echo 'AllowTcpForwarding no' >> /etc/ssh/sshd_config
-		WTAF
-	end
+  bash "add AllowTcpForwarding no" do
+    code <<-WTAF
+    grep -q 'AllowTcpForwarding no' /etc/ssh/sshd_config || echo 'AllowTcpForwarding no' >> /etc/ssh/sshd_config
+    WTAF
+  end
 end
 
 #restart the sshd process so the next login will be duo protected
 bash "restart sshd process" do
-	code "kill -HUP `cat /var/run/sshd.pid`"
+  code "kill -HUP `cat /var/run/sshd.pid`"
 end
 
